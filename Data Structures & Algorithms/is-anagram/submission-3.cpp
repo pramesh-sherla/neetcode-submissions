720ffc44c13ac9bcb35001a9b37a84bc1b5b00e3
pramesh-sherla <66243912+pramesh-sherla@users.noreply.github.com>
2026-06-17 18:35:49 +0900

    Add: is-anagram - submission-3

---
+++ Data Structures & Algorithms/is-anagram/submission-3.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        if (s.length() != t.length())
+        {
+            return false;
+        }
+        else
+        {
+            sort(s.begin(), s.end());
+            sort(t.begin(), t.end());
+            return s==t;
+
+        }
+    }
+};
